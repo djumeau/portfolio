8759e403d415b4b1d6d73deb96c7a828e333ff86
David Jumeau <djumeau@gmail.com>
2023-11-08 17:47:11 -0500

    Initial

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function runTypingEffect() {
+    const text = "Welcome to GeminiLearning.com.";
+    const typingElement = document.getElementById('typing-text');
+
+    const typingDelay = 150;
+
+    typeText(text, typingElement, typingDelay);
+}
+
+function typeText(text, typingElement, delay) {
+    for (let i=0; i < text.length; i++ ) {
+        setTimeout(() => {
+            typingElement.textContent += text.charAt(i);
+        }, delay * i);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', runTypingEffect);
